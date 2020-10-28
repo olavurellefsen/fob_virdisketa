5fcf89cf60268fcdcef3271e0185ae0815f18161
olavurellefsen <oe@tokni.com>
2020-10-28 19:58:10 +0000

    Add Auth0 and Apollo/Hasura wrappers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'spectre.css'
+import './index.css'
+import App from './App'
+import { Auth0Provider } from '@auth0/auth0-react'
+import { GraphQLProvider } from './apollo'
 
-import 'spectre.css';
-import './index.css';
-
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH0_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+    redirectUri={window.location.origin}
+    audience="hasura"
+  >
+    <GraphQLProvider>
+      <App />
+    </GraphQLProvider>
+  </Auth0Provider>,
+  document.getElementById('root')
+)
